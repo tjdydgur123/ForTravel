8a8b8dfe64cff5596fb2af57e95a4b2a9f84d447
YongHyuk Seo <tjdydgur123@gmail.com>
2022-04-10 15:08:20 -0700

    reverted all the previous works

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,6 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
-import UploadProductPage from "./views/UploadProductPage/UploadProductPage";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -22,14 +21,9 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route
-            exact
-            path="/product/upload"
-            component={Auth(UploadProductPage, true)}
-          />
         </Switch>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </Suspense>
   );
 }
