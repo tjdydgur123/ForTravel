b2e579f064f7df5a6265f8e10f3d494611adc207
YongHyuk Seo <tjdydgur123@gmail.com>
2022-04-12 10:19:18 -0700

    Revert "add dropzone in the UploadProductPage"
    This reverts commit ebfff59a74ac618c2f1c2407e9c514d2be4068c1.

--- server/index.js
+++ server/index.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/users", require("./routes/users"));
+app.use("/api/product", require("./routes/product"));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
